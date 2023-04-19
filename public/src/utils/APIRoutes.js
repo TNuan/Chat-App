8abedfd1c01f30a071676fb2179336e31900dc6b
Nguyễn Thành Luân <minibike090@gmail.com>
2023-04-20 00:02:25 +0700

    Chat message setup.

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -3,3 +3,5 @@ export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const allUsersRoute = `${host}/api/auth/allusers`
 export const setAvatarRoute = `${host}/api/auth/setavatar`
+export const sendMessageRoute = `${host}/api/message/addmsg`
+export const getAllMessageRoute = `${host}/api/message/getmsg`
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose') 
 const userRoutes = require('./routes/userRoutes')
-
+const messageRoute = require('./routes/messageRoute')
 const app = express()
 require('dotenv').config()
 
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', userRoutes)
+app.use('/api/message', messageRoute)
 
 mongoose
     .connect(process.env.MONGO_URL, {
